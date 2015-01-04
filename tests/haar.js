55f1f203856de4a478f45fb3866b9f0c9e1c85f9
Maximilian Klein <dasmaxl@gmail.com>
2015-01-04 14:50:47 +0100

    tested haar wavelets added triangle and rectangle generator

--- tests/haar.js
+++ tests/haar.js
@@ -9,34 +9,27 @@ var Processing = require("../lib/processing.js");
 var Signals = require("../lib/utils/signals.js");
 
 describe("Haar Wavelet Transform", function(){
-  /*it("should detect the frequency of the sine generator", function(){
-    // generate signal of 440 hertz
-    var signal = Generator
-    .sine({frequency: 440})
-    .create({length: 100, sampling: 4400});
-    
-    // calculate the dominant frequency of the signal using dft
-    var domFreq = Transform
-    .toSpectrum(signal,{method: "haar"})
-    .dominantFrequency();
-    domFreq.frequency.should.equal(440);
-  });*/
-/*  it("should detect different frequencies", function(){
+  it("should detect different (rectangle) frequencies", function(){
     //for(var i=1; i<4; i++) {
     for(var i=1; i<4; i++) {
-      var rect = Signals.rectangle(Math.pow(2,4-i));
-      cosFunc = _.map(_.range(8), function (idx) {
-        return rect(idx);
-      });
-      console.log("func");
-      console.log(cosFunc);
+      var freq = Math.pow(2,i-1);
+      var rectFunc = Generator.rectangle({frequency:freq})
+        .create({sampling:8,length:8})
+        .values();
       // calculate the dominant frequency of the signal using dft
-      var spec = Transform.toSpectrum(cosFunc, {method: "haar", sampling:8});
-      console.log(spec);
       var domFreq = Transform
-      .toSpectrum(cosFunc,{method: "haar", sampling:8})
+      .toSpectrum(rectFunc,{method: "haar", sampling:8})
       .dominantFrequency();
-      domFreq.frequency.should.equal(i)
+      domFreq.frequency.should.equal(Math.pow(2,i - 1))
     }
-  });*/
+  });
+  it("should transform an impuls correctly", function(){
+    // example taken from http://en.wikipedia.org/wiki/Discrete_wavelet_transform#Comparison_with_Fourier_transform
+    var impuls = [1,0,0,0];
+    var spec = Transform
+      .toSpectrum(impuls,{method:"haar", sampling: 4})
+      .amplitudes();
+    var areEqual = Processing.equal(spec, [0.25,0.25,0.5,0]);
+    areEqual.should.be.true;
+  })
 });
