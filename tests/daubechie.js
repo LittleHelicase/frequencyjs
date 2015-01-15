87873fdc6c95ff69485ae96711fb600fbbf64ea0
Maximilian Klein <dasmaxl@gmail.com>
2015-01-15 23:44:40 +0100

    trying to implement reverse transform

--- tests/daubechie.js
+++ tests/daubechie.js
@@ -10,9 +10,19 @@ var Signals = require("../lib/utils/signals.js");
 
 describe("Daubechies Wavelet Transform", function(){
   it("should equal haar for D2", function(){
-    var sineSig = Generator.sine().create({length:128});
+    var sineSig = Generator.sine().create({length:1024});
     var haarSpec = Transform.toSpectrum(sineSig, {method: "haar",sampling:8});
     var daubSpec = Transform.toSpectrum(sineSig, {method: "daubechies", taps:2,sampling:8});
     haarSpec.dominantFrequency().frequency.should.equal(daubSpec.dominantFrequency().frequency);
+  });
+  it("should be invertible", function(){
+    var signal = Generator.sines([
+      {frequency: 40, amplitude: 0.5},
+      {frequency: 12, amplitude: 0.8},
+      ]).create({length:128, sampling: 100});
+    var spec = Transform.toSpectrum(signal,{method:"daubechies"});
+    var signalBack = Transform.toSignal(spec,{method:"daubechies"});
+    var areEqual = Processing.equal(signal, signalBack);
+    areEqual.should.be.true;
   });
 });
--- tests/haar.js
+++ tests/haar.js
@@ -6,7 +6,7 @@ chai.should();
 var Generator = require("../lib/generator.js");
 var Transform = require("../lib/transform.js");
 var Processing = require("../lib/processing.js");
-var Signals = require("../lib/utils/signals.js");
+var Spectrum = require("../lib/spectrum.js");
 
 describe("Haar Wavelet Transform", function(){
   it("should detect different (rectangle) frequencies", function(){
@@ -14,11 +14,12 @@ describe("Haar Wavelet Transform", function(){
     for(var i=1; i<4; i++) {
       var freq = Math.pow(2,i-1);
       var rectFunc = Generator.rectangle({frequency:freq})
-        .create({sampling:8,length:8})
-        .values();
+        .create({sampling:8,length:8});
+      var spec = Transform
+        .toSpectrum(rectFunc,{method: "haar"})
       // calculate the dominant frequency of the signal using dft
       var domFreq = Transform
-      .toSpectrum(rectFunc,{method: "haar", sampling:8})
+      .toSpectrum(rectFunc,{method: "haar"})
       .dominantFrequency();
       domFreq.frequency.should.equal(Math.pow(2,i - 1))
     }
@@ -31,5 +32,22 @@ describe("Haar Wavelet Transform", function(){
       .amplitudes();
     var areEqual = Processing.equal(spec, [0.25,0.25,0.5,0]);
     areEqual.should.be.true;
-  })
+  });
+  it("should be invertible", function(){
+    var spec = Spectrum([0.25,0.25,0.5,0],{timeDependent: true, sampling:4});
+    var signal = Transform
+      .toSignal(spec,{method:"haar"});
+    signal.sampling.should.be.equal(4);
+    console.log(signal);
+    var areEqual = Processing.equal(signal, [1,0,0,0]);
+    areEqual.should.be.true;
+/*    var signal = Generator.sines([
+      {frequency: 40, amplitude: 0.5},
+      {frequency: 12, amplitude: 0.8},
+      ]).create({length:128, sampling: 100});
+    var spec = Transform.toSpectrum(signal,{method:"haar"});
+    var signalBack = Transform.toSignal(spec,{method:"haar"});
+    var areEqual = Processing.equal(signal, signalBack);
+    areEqual.should.be.true;*/
+  });
 });
